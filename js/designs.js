c1fb1a65dbc91935e709cbcc1cfdf5604cce7e70
Sean Morrison <sean1972@gmail.com>
2018-05-19 10:23:43 -0700

    Added jQuery to index.html and assigned values to the #sizePicker form

--- js/designs.js
+++ js/designs.js
@@ -1,9 +1,21 @@
 // Select color input
 // Select size input
-var height, width, color;
+const height, width, color;
 
 // When size is submitted by the user, call makeGrid()
+// select the form by it's ID and submit it
+$('#sizePicker').submit(function(event) {
+  event.preventDefault();
+  height = $('#inputHeight').val(); // get value of grid height input
+  width = $('#inputWidth').val(); // get value of grid width input
+  makeGrid(height, width);
+});
 
 function makeGrid() {
+  // clear the screen
+  $('table tr').remove();
+
   // Your code goes here!
-}
+
+
+};
